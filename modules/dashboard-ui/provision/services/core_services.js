2764bbc1fb207e06f41d12280703fcad5e1a11fc
mikehajj <mike.hajj@gmail.com>
2015-04-17 12:11:18 +0300

    updated environment record, added new services section containing services records and hosts.

---
+++ modules/dashboard-ui/provision/services/core_services.js
@@ -0,0 +1,119 @@
+var core_services = [
+	{
+		"name": "controller",
+		"maxPoolSize": 100,
+		"authorization": true,
+		"port": 4000,
+		"requestTimeout": 30,
+		"requestTimeoutRenewal": 0
+	},
+	{
+		"name": "urac",
+		"extKeyRequired": true,
+		"port": 4001,
+		"apis": []
+	},
+	{
+		"name": "oauth",
+		"extKeyRequired": true,
+		"port": 4002,
+		"apis": []
+	},
+	{
+		"name": "dashboard",
+		"extKeyRequired": true,
+		"port": 4003,
+		"apis": []
+	},
+	{
+		"name": "agent",
+		"extKeyRequired": false,
+		"port": 4004,
+		"requestTimeoutRenewal": 10,
+		"requestTimeout": 30,
+		"apis": []
+	},
+	{
+		"name": "example01",
+		"extKeyRequired": false,
+		"port": 4010,
+		"apis": []
+	},
+	{
+		"name": "example02",
+		"extKeyRequired": false,
+		"port": 4011,
+		"apis": []
+	},
+	{
+		"name": "example03",
+		"extKeyRequired": false,
+		"port": 4012,
+		"apis": []
+	},
+	{
+		"name": "example04",
+		"extKeyRequired": false,
+		"port": 4013,
+		"apis": []
+	},
+	{
+		"name": "contactUs",
+		"extKeyRequired": false,
+		"port": 4015,
+		"apis": []
+	}
+];
+
+var core_hosts = [
+	{
+		"env": "dev",
+		"name": "controller",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "urac",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "oauth",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "dashboard",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "agent",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "example01",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "example02",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "example03",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "example04",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "contactUs",
+		"ip": "127.0.0.1"
+	}
+];
