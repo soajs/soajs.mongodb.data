64581987343218c38dba9968acbec727eb224e81
antoinehage <me@antoinehage.com>
2015-04-19 16:00:52 -0400

    fixed urac test data

--- modules/urac/import.sh
+++ modules/urac/import.sh
@@ -3,6 +3,8 @@
 pushd ./provision
 mongo ./product.js
 mongo ./tenant.js
+mongo ./environment.js
+mongo ./services.js
 popd
 pushd ./urac
 mongo ./urac.js
---
+++ modules/urac/provision/services/core_services.js
@@ -0,0 +1,21 @@
+var core_services = [
+    {
+        "name": "urac",
+        "extKeyRequired": true,
+        "port": 4001,
+        "apis": []
+    }
+];
+
+var core_hosts = [
+	{
+		"env": "dev",
+		"name": "controller",
+		"ip": "127.0.0.1"
+	},
+    {
+        "env": "dev",
+        "name": "urac",
+        "ip": "127.0.0.1"
+    }
+];
