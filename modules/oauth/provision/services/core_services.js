97a082f33f6e10d4a50eac8f4f89b743bb7975a3
antoinehage <me@antoinehage.com>
2015-04-19 16:11:16 -0400

    added oauth test data

---
+++ modules/oauth/provision/services/core_services.js
@@ -0,0 +1,21 @@
+var core_services = [
+    {
+        "name": "oauth",
+        "extKeyRequired": true,
+        "port": 4002,
+        "apis": []
+    }
+];
+
+var core_hosts = [
+	{
+		"env": "dev",
+		"name": "controller",
+		"ip": "127.0.0.1"
+	},
+    {
+        "env": "dev",
+        "name": "oauth",
+        "ip": "127.0.0.1"
+    }
+];
