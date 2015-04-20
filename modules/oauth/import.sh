e8da91cad13e8e734133f1b0e58316078a7c5b7d
mikehajj <mike.hajj@gmail.com>
2015-04-20 10:42:38 +0300

    updated soajs.oauth module configuration file and test cases. updated soajs.mongodb.data/module/oauth to provide the new provisioning

--- modules/oauth/import.sh
+++ modules/oauth/import.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 pushd ./provision
+mongo ./environment.js
 mongo ./product.js
 mongo ./tenant.js
 mongo ./oauth.js
+mongo ./services.js
 popd
---
+++ modules/oauth/provision/services/core_services.js
@@ -0,0 +1,25 @@
+var core_services = [
+	{
+		"name": "oauth",
+		"extKeyRequired": true,
+		"port": 4002,
+		"apis": [
+			{
+				'l': "Create Token",
+				'v': "/token"
+			},
+			{
+				'l': "Kill Token",
+				'v': "/kill"
+			}
+		]
+	}
+];
+
+var core_hosts = [
+	{
+		"env": "dev",
+		"name": "oauth",
+		"ip": "127.0.0.1"
+	}
+];
