bdd98c2d3ba4a1ab93b8570f2839011c05e95a46
mikehajj <mike.hajj@gmail.com>
2015-04-20 15:10:25 +0300

    updated examples data import, services config and their tests.

--- modules/examples/import.sh
+++ modules/examples/import.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 pushd ./provision
+mongo ./environment.js
 mongo ./oauth_urac.js
 mongo ./provision.js
 mongo ./product.js
+mongo ./services.js
 popd
 pushd ./urac
 mongo ./urac.js
---
+++ modules/examples/provision/services/core_services.js
@@ -0,0 +1,208 @@
+var core_services = [
+	{
+		"name": "controller",
+		"maxPoolSize": 100,
+		"authorization": true,
+		"port": 4000,
+		"requestTimeout": 30,
+		"requestTimeoutRenewal": 0
+	},
+	{
+		"name": "oauth",
+		"extKeyRequired": true,
+		"port": 4002,
+		"apis": [
+			{
+				'l': "Create Token",
+				'v': "/token"
+			},
+			{
+				'l': "Kill Token",
+				'v': "/kill"
+			}
+		]
+	},
+	{
+		"name": "urac",
+		"extKeyRequired": true,
+		"port": 4001,
+		"apis": [
+			{
+				"l": "Change Email",
+				"group": "My Account",
+				'v': "/account/changeEmail"
+			},
+			{
+				"l": "Change Password",
+				"group": "My Account",
+				'v': "/account/changePassword"
+			},
+			{
+				"l": "Edit Profile",
+				"group": "My Account",
+				'v': "/account/editProfile"
+			},
+			{
+				"l": "Get User Info",
+				"group": "My Account",
+				"groupMain": true,
+				'v': "/account/getUser"
+			},
+			{
+				"l": "Add new User",
+				"group": "Administration",
+				'v': "/admin/addUser"
+			},
+			{
+				"l": "Change User Status",
+				"group": "Administration",
+				'v': "/admin/changeUserStatus"
+			},
+			{
+				"l": "Edit User Record",
+				"group": "Administration",
+				'v': "/admin/editUser"
+			},
+			{
+				"l": "Add new Group",
+				"group": "Administration",
+				'v': "/admin/group/add"
+			},
+			{
+				"l": "Add Users to Group",
+				"group": "Administration",
+				'v': "/admin/group/addUsers"
+			},
+			{
+				"l": "Delete Group",
+				"group": "Administration",
+				'v': "/admin/group/delete"
+			},
+			{
+				"l": "Edit Group",
+				"group": "Administration",
+				'v': "/admin/group/edit"
+			},
+			{
+				"l": "List Groups",
+				"group": "Administration",
+				'v': "/admin/group/list"
+			},
+			{
+				"l": "List Users",
+				"group": "Administration",
+				"groupMain": true,
+				'v': "/admin/listUsers"
+			},
+			{
+				"l": "Edit Multiple Groups",
+				"group": "Administration",
+				'v': "/admin/user/editGroups"
+			},
+			{
+				"l": "Validate Change Email",
+				"group": "Guest",
+				'v': "/changeEmail/validate"
+			},
+			{
+				"l": "Forgot Password",
+				"group": "Guest",
+				'v': "/forgotPassword"
+			},
+			{
+				"l": "Register",
+				"group": "Guest",
+				'v': "/join"
+			},
+			{
+				"l": "Validate Register",
+				"group": "Guest",
+				'v': "/join/validate"
+			},
+			{
+				"l": "Login",
+				"group": "Guest",
+				"groupMain": true,
+				'v': "/login"
+			},
+			{
+				"l": "Logout",
+				"group": "Guest",
+				'v': "/logout"
+			},
+			{
+				"l": "Reset Password",
+				"group": "Guest",
+				'v': "/resetPassword"
+			}
+		]
+	},
+	{
+		"name": "example01",
+		"extKeyRequired": false,
+		"port": 4010,
+		"apis": []
+	},
+	{
+		"name": "example02",
+		"extKeyRequired": false,
+		"port": 4011,
+		"apis": []
+	},
+	{
+		"name": "example03",
+		"extKeyRequired": false,
+		"port": 4012,
+		"apis": []
+	},
+	{
+		"name": "example04",
+		"extKeyRequired": false,
+		"port": 4013,
+		"apis": []
+	},
+	{
+		"name": "contactUs",
+		"extKeyRequired": false,
+		"port": 4015,
+		"apis": []
+	}
+];
+
+var core_hosts = [
+	{
+		"env": "dev",
+		"name": "controller",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "urac",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "oauth",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "example01",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "example02",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "example03",
+		"ip": "127.0.0.1"
+	},
+	{
+		"env": "dev",
+		"name": "example04",
+		"ip": "127.0.0.1"
+	}
+];
