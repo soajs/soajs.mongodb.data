0d79c9f9ed2b6802a316b370de032c7d1dc548c4
mikehajj <mike.hajj@gmail.com>
2015-04-23 12:20:31 +0300

    removed unneeded folder

--- modules/dashboard-ui/urac/groups/administrator.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var administrator = {
-	"_id": ObjectId('55128764e603d7e01ab1688f'), 
-	"locked" : true,
-	"code": "administrator",
-	"name": "administrator",	 
-	"description": "this is the administrator group"
-};
