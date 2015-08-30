849ac280164ba180b3a5f28d9ea632802611978b
mikehajj <mike.hajj@gmail.com>
2015-08-31 01:09:17 +0300

    updated provisioning data and dashboard ui calls to accommodate roaming.

--- modules/dashboard/provision/products/locked.js
+++ modules/dashboard/provision/products/locked.js
@@ -25,23 +25,6 @@ var locked = {
 			"_TTL": 86400000 // 24 hours
 		},
 		{
-			"code": "DSBRD_MAIN",
-			"name": "Main package",
-			"locked": true,
-			"description": "this is the main dashboard product package.",
-			"acl": {
-				"urac": {},
-				"agent": {
-					"access": true
-				},
-				"oauth": {},
-				"dashboard": {
-					"access": true
-				}
-			},
-			"_TTL": 86400000 // 24 hours
-		},
-		{
 			"code": "DSBRD_OWNER",
 			"name": "Dashboard Owner Package",
 			"description": "This package provides full access to manage the dashboard and urac features.",
