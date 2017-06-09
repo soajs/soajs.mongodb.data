dafc3eddbab109c6998ec53b6263bc72a1cbcfe4
mikehajj <mike.hajj@gmail.com>
2017-06-09 19:51:30 +0300

    updated ACL recipe

--- modules/oauth/provision/products/test.js
+++ modules/oauth/provision/products/test.js
@@ -1,29 +1,43 @@
 'use strict';
 
 var testProduct = {
-    "_id": ObjectId("50d2cb5fc04ce51e06000001"),
-    "code" : "TPROD",
-    "name" : "Test Product",
-    "description" : "this is a description for test product",
-    "packages" : [
-        {
-            "code" : "TPROD_BASIC",
-            "name" : "basic package",
-            "description" : "this is a description for test product basic package",
-            "acl" : {
-                "urac" : {},
-	            "dashboard": {},
-	            "oauth" : {
-                	"access": false,
-                	"apis":{
-                		"/accessToken/:token":{ "access": true},
-                		"/refreshToken/:token":{ "access": true},
-                		"/tokens/:userId":{ "access": true},
-                		"/authorization/:clientId":{ "access": true}
-	                }
-	            }
-            },
-            "_TTL" : 86400000 // 24 hours
-        }
-    ]
+	"_id": ObjectId("50d2cb5fc04ce51e06000001"),
+	"code": "TPROD",
+	"name": "Test Product",
+	"description": "this is a description for test product",
+	"packages": [
+		{
+			"code": "TPROD_BASIC",
+			"name": "basic package",
+			"description": "this is a description for test product basic package",
+			"acl": {
+				"dev": {
+					"urac": {},
+					"dashboard": {},
+					"oauth": {
+						"access": false,
+						"get": {
+							"apis": {
+								"/authorization": {"access": false}
+							}
+						},
+						"post": {
+							"apis": {
+								"/token": {"access": false}
+							}
+						},
+						"delete": {
+							"apis": {
+								"/accessToken/:token": {"access": true},
+								"/refreshToken/:token": {"access": true},
+								"/tokens/user/:userId": {"access": true},
+								"/tokens/tenant/:clientId": {"access": true}
+							}
+						}
+					}
+				}
+			},
+			"_TTL": 86400000 // 24 hours
+		}
+	]
 };
