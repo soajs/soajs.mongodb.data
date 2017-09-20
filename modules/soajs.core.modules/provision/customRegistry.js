09ca8c94e00fea67c907fb0327261fa259acff7a
mikehajj <mike.hajj@gmail.com>
2017-09-20 14:09:11 +0300

    renamed customRegistry --> custom_registry ( same naming convention as other collections in db )

--- modules/soajs.core.modules/provision/customRegistry.js
+++ modules/soajs.core.modules/provision/customRegistry.js
@@ -5,14 +5,14 @@ for (var i = 0; i < files.length; i++) {
     load(files[i].name);
 }
 
-provDb.customRegistry.drop();
+provDb.custom_registry.drop();
 
 var records = [];
 var index = 0;
 
-records.push(customRegistry[index++]);
+records.push(custom_registry[index++]);
 
-provDb.customRegistry.insert(records);
+provDb.custom_registry.insert(records);
 
 /* Indexes for products */
-provDb.customRegistry.ensureIndex({ code: 1 }, { unique: true });
+provDb.custom_registry.ensureIndex({ code: 1 }, { unique: true });
